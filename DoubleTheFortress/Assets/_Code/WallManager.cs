d0a23efca77991c1c66f4fa1a4745f858e37a804
AmerikeAzul14 <nikot92089@upshopt.com>
2022-09-01 15:47:49 -0500

    Input system

---
+++ DoubleTheFortress/Assets/_Code/WallManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallManager : MonoBehaviour
+{
+    [Header("Dependendices")]
+    [SerializeField] private GameObject _wallPrefab;
+
+    [Header("Settings")]
+    [SerializeField] private float _health;
+}
