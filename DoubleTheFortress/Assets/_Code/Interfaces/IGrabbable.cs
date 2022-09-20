28f6b743bdd41e274081867961bd3bd6ded00f82
Diana Ramos/ Dinorekkos <diana_ramos_g@hotmail.com>
2022-09-20 14:01:44 -0500

    dino changes

--- DoubleTheFortress/Assets/_Code/Interfaces/IGrabbable.cs
+++ DoubleTheFortress/Assets/_Code/Interfaces/IGrabbable.cs
@@ -1,12 +1,23 @@
 using UnityEngine;
+using System;
+
 
 public interface IGrabbable 
 {
-
     Vector3 Position
     {
         get;
         set;
     }
-    
+
+    Vector3 Rotation
+    {
+        get;
+        set;
+    }
+
+
+   public void ReposObject();
+   public void DoAction();
+
 }
