63f51a7327e19295fce60f537a7337dff19c520f
Diana/Dinorekkos <diana_ramos_g@hotmail.com>
2022-10-07 18:49:44 -0500

    Inputs para inventario ( supuestametne fuincionales) hay que testear

--- DoubleTheFortress/Assets/_Code/Inventory/InventoryController.cs
+++ DoubleTheFortress/Assets/_Code/Inventory/InventoryController.cs
@@ -1,28 +1,79 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.XR.Interaction.Toolkit;
 public class InventoryController : MonoBehaviour
 {
+ 
+
     [SerializeField] private GameObject hammerHand;
     [SerializeField] private GameObject gunHand;
     [SerializeField] private GameObject disfriHand;
     
+    private bool hasObjectSelected = false;
+    
+    private XRIDefaultInputActions inputActions;
+    private XRIDefaultInputActions.XRIRightHandInteractionActions rightHandActions;
+
+    
     void Start()
     {
-        
+        inputActions = new XRIDefaultInputActions();
+        rightHandActions = inputActions.XRIRightHandInteraction;
+        rightHandActions.Select_Hammer.performed += ctx => SelectHammer();
+        rightHandActions.Select_Weapon.performed += ctx => SelectWeapon();
+        InitializeHandObjects();
     }
 
-    void Update()
+    void InitializeHandObjects()
     {
+        hammerHand.SetActive(false);
+        gunHand.SetActive(false);
+        disfriHand.SetActive(false);
+    }
+    void SelectHammer()
+    {
+        if (hasObjectSelected)
+        {
+            InitializeHandObjects();
+            hasObjectSelected = false;
+        }
         
+        //Handle Hammer Selection
+        if(hammerHand.activeSelf)
+        {
+            hammerHand.SetActive(false);
+            hasObjectSelected = false;
+            
+        }
+        else
+        {
+            hammerHand.SetActive(true);
+            hasObjectSelected = true;
+        }
     }
-
-    void HandleGameInventory(GameObject objHand, bool active)
+    void SelectWeapon()
     {
-        objHand.SetActive(active);
+        if (hasObjectSelected)
+        {
+            InitializeHandObjects();
+            hasObjectSelected = false;
+        }
+        //Handle Weapon Selection
+        if(gunHand.activeSelf)
+        {
+            gunHand.SetActive(false);
+            hasObjectSelected = false;
+
+        }
+        else
+        {
+            gunHand.SetActive(true);
+            hasObjectSelected = true;
+
+        }
     }
-    
-    
-    
 }
+    
+    
+
