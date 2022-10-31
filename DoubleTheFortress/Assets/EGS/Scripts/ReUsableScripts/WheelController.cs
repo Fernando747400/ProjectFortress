25a2864f8b280c8498519b08b7cdc524454a8a51
Dino <diana_ramos_g@hotmail.com>
2022-10-31 17:17:47 -0600

    changes cannon

--- DoubleTheFortress/Assets/EGS/Scripts/ReUsableScripts/WheelController.cs
+++ DoubleTheFortress/Assets/EGS/Scripts/ReUsableScripts/WheelController.cs
@@ -4,29 +4,59 @@ using UnityEngine;
 
 public class WheelController : MonoBehaviour
 {
+    [SerializeField] private HeavyWeaponRotate_VR _heavyWeaponRotateVR;
     [SerializeField] private Two_HandsHolder rightHolder;
     [SerializeField] private Two_HandsHolder leftHolder;
+
+
+    private Vector3 _distanceBetweenHands;
+    private Vector3 _middlePoint;
+    private bool _canMoveCannon;
     void Start()
     {
         rightHolder.OnGrabbed += HandleWheelInteraction;
         rightHolder.OnReleased += HandleWheelInteraction;
+        
         leftHolder.OnReleased += HandleWheelInteraction;
         leftHolder.OnGrabbed += HandleWheelInteraction;
+
+        leftHolder.OnHandsOut += HandleWheelInteraction;
+        rightHolder.OnHandsOut += HandleWheelInteraction;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (rightHolder.Hand != null && leftHolder.Hand != null)
+        {
+            _distanceBetweenHands = rightHolder.Hand.gameObject.transform.position - leftHolder.Hand.gameObject.transform.position;
+
+            _middlePoint = _distanceBetweenHands * 0.5f;
+
+            if (_canMoveCannon)
+            {
+                _heavyWeaponRotateVR.LookAtRotate(_middlePoint);
+            }
+            // Debug.DrawRay();
+            // Debug.DrawLine(rightHolder.Hand.gameObject.transform.position, leftHolder.Hand.gameObject.transform.position);
+        }
     }
 
     void HandleWheelInteraction()
     {
         if (leftHolder.IsGrabbing && rightHolder.IsGrabbing)
         {
-            print("Ambas Manos estan agarrando el wheel");
-
+            _canMoveCannon = true;
+            print("CAN MOVE CANNON = " + _canMoveCannon);
         }
+        else
+        {
+            _canMoveCannon = false;
+        }
+
     }
+    
+    
+    
     
 }
--- DoubleTheFortress/Assets/EGS/Scripts/XR/Two_HandsHolder.cs
+++ DoubleTheFortress/Assets/EGS/Scripts/XR/Two_HandsHolder.cs
@@ -6,12 +6,19 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class Two_HandsHolder : XRGrabInteractable
 {
+    private HandController_XR _hand;
+
     public Action OnGrabbed;
     public Action OnReleased;
+    public Action OnHandsOut;
 
     private bool _isGrabbing;
-    
-    
+
+    public HandController_XR Hand
+    {
+        get => _hand;
+    }
+
     public bool IsGrabbing => _isGrabbing;
 
     protected override void Awake()
@@ -19,26 +26,46 @@ public class Two_HandsHolder : XRGrabInteractable
         base.Awake();
         onSelectEntered.AddListener(Grab);
         onSelectExited.AddListener(Drop);
-        
-    }
 
-    
+    }
     protected virtual void Grab(XRBaseInteractor interactor)
     {
         _isGrabbing = true;
+        _hand.HandleHandsVisible(!_isGrabbing);
         OnGrabbed?.Invoke();
     }
 
     protected virtual void Drop(XRBaseInteractor interactor)
     {
         _isGrabbing = false;
+        if(_hand!= null) _hand.HandleHandsVisible(!_isGrabbing);
         OnReleased?.Invoke();
 
     }
-
-
+    
     private void OnTriggerStay(Collider other)
     {
-        
+        if (other.CompareTag("Hand"))
+        {
+            // Debug.Log("Hand is in the handle");
+            _isGrabbing = true;
+            _hand = other.GetComponent<HandController_XR>();
+            OnHandsOut?.Invoke();
+
+        }
     }
+    
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Hand"))
+        {
+            // Debug.Log("Hand is out the handle");
+            _isGrabbing = false;
+            _hand.HandleHandsVisible(!_isGrabbing);
+            OnHandsOut?.Invoke();
+            _hand = null;
+        }
+    }
+    
+    
 }
