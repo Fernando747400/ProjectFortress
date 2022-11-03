dd0d22266f99bb20722df0095421b61e40f61199
Sr Almeja <cdmx1895@amerike.edu.mx>
2022-11-03 17:00:35 -0600

    menu2

--- DoubleTheFortress/Assets/EGS/Scripts/UI/MenuManager.cs
+++ DoubleTheFortress/Assets/EGS/Scripts/UI/MenuManager.cs
@@ -7,12 +7,13 @@ using UnityEngine;
 public class MenuManager : MonoBehaviour
 {
     public string Scene;
+    public bool IsExitButton = false;
     private void OnTriggerEnter(Collider other)
     {
-        
         if (other.CompareTag("Bullet"))
         {
-            SceneTransition.Instance.LoadScene(Scene);
+            if (!IsExitButton) SceneTransition.Instance.LoadScene(Scene);
+            else SceneTransition.Instance.Quit();
         }
     }
 }
