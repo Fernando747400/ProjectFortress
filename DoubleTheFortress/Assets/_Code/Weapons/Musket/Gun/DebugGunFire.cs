5df84a4accd7a77eec87bff19ef4f5579f057bf3
Raymundo Mosqueda <iracoon2014@gmail.com>
2022-11-04 11:56:47 -0600

    Musket, Cannon & Walls Layers & Tag interactions

--- DoubleTheFortress/Assets/_Code/Weapons/Musket/Gun/DebugGunFire.cs
+++ DoubleTheFortress/Assets/_Code/Weapons/Musket/Gun/DebugGunFire.cs
@@ -5,14 +5,13 @@ public class DebugGunFire :  GeneralAgressor
 {
     [SerializeField] float bulletSpeed;
     [SerializeField] float maxDistance;
-    [SerializeField] GameObject hitMarkerRed;
-    [SerializeField] GameObject hitMarkerBlue;
+    // [SerializeField] private GameObject hitMarkerRed;
+    [SerializeField] private GameObject hitMarkerBlue;
+    [SerializeField] private LayerMask layers;
     private float _bulletMass;
-    protected float _travelTime;
+    private float _travelTime;
     private bool _canFire = true;
     private Vector3 _hitPosition;
-    private Vector3 _gravity = new Vector3(0, 9.8f, 0);
-    private Vector3 localForward = new Vector3(0, 0, 1);
     
     //saves the position the gun was... 
     //in when it fired the  hitScan
@@ -37,7 +36,7 @@ public class DebugGunFire :  GeneralAgressor
         if (!Physics.Raycast( transform.position, transform.forward, out RaycastHit hitScan, maxDistance,
                 Physics.DefaultRaycastLayers)) return;
         Debug.DrawLine(transform.position,hitScan.point,Color.red,10f);
-        Instantiate(hitMarkerRed, hitScan.point, Quaternion.identity);
+        // Instantiate(hitMarkerRed, hitScan.point, Quaternion.identity);
         _hitPosition = hitScan.point;
         _travelTime = hitScan.distance / (bulletSpeed) * Time.fixedDeltaTime;
         _canFire = false;
@@ -57,10 +56,8 @@ public class DebugGunFire :  GeneralAgressor
         _savedFirePosition = transform.position;
         Vector3 simulatedHitPos = _hitPosition - _savedFirePosition;
         //Vector3 simulatedHitPos = (_hitPosition - new Vector3(0, _gravity.y / _travelTime / Time.fixedDeltaTime, 0)) - _savedFirePosition;
-        Debug.Log(_hitPosition);
-        
         Physics.Raycast(_savedFirePosition, simulatedHitPos.normalized,out RaycastHit simulatedHit,
-            maxDistance, Physics.DefaultRaycastLayers);
+            maxDistance, layers);
         Debug.DrawLine(_savedFirePosition,simulatedHit.point,Color.cyan);
         Instantiate(hitMarkerBlue, simulatedHit.point, Quaternion.identity);
         
